380f5fe045c4f080488f812efa3fd7eb2ce44cbf
Kathryn Helsley <kathrynhelsley@Kathryns-MacBook-Pro-2.local>
2019-04-02 15:52:31 -0700

    BikeWise API data in process

--- assets/cbear.js
+++ assets/cbear.js
@@ -24,16 +24,18 @@ function initMap() {
         });
     } else {
         //  Browser doesn't support Geolocation
-        handleLocationError(false , infoWindow, map.getCenter());
+        handleLocationError(false, infoWindow, map.getCenter());
     }
 
     function handleLocationError(BrowserHasGeoLocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(BrowserHasGeoLocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
     }
+
+
 
     // listen for click on map
     // google.maps.event.addListener(map, 'click',
@@ -43,29 +45,104 @@ function initMap() {
     // });
 
 
+    // BikeWise AJAX call for CitySearch
+
+    var $city = $("#cityEntr");
+
+    $("#submitSearch").on("click", function (event) {
+        event.preventDefault();
+        markers = [];
+        var city = $city.val().trim();
+
+        // array of markers for BW
+        var BWmarkers = [];
+        //     {coords: { lat: "", lng:  ""},
+        //      content: "",
+        //     },
+        // ];
+
+
+        var queryURL = "https://bikewise.org:443/api/v2/locations?" + city + "proximity_square=10"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            for (var i = 0; i < 11; i++) {
+                var dataBW = response.features[i];
+                if (dataBW.geometry.coordinates[0]) {
+
+                    var coords = {lat: parseFloat(dataBW.geometry.coordinates[1]), lng: parseFloat(dataBW.geometry.coordinates[0])};
+                    BWmarkers.push(coords);
+
+
+    
+                    markers.push({coords});
+                    
+                    
+                    // function swap(BWmarkers, i, j) {
+                        //     var temp = BWmarkers[i];
+                        //     BWmarkers[i] = BWmarkers[j];
+                        //     BWmarkers[j] = temp;
+                        //   };
+                        
+                        // function george() {
+                            //     // var length = arr.length;
+                            //     // while (length) {
+                                //     for (var i = 0; i < BWmarkers.length; i++) {
+                                    //         var num = BWmarkers[i],
+                                    //             nextNum = BWmarkers[i + 1];
+                                    
+                                    //         BWmarkers[i] = nextNum;
+                                    //         BWmarkers[i + 1] = num;
+                                    //     }
+                                    // }
+                                }
+                                createMarkers();
+                                
+                                
+                                // create new map
+                                // var map = new google.maps.Map(document.getElementById('map'), options);
+                                // infoWindow = new google.maps.InfoWindow;
+                            }
+                            
+                        });
+                    });
+                    var markers = [
+                        // {
+                            //     coords: { lat: 34.0622, lng: -118.4450 },
+                            //     // content: '<h1>UCLA Extension</h1>'
+                            // },
+                            // {
+                                //     coords: { lat: 34.0780, lng: -118.4741 },
+                                //     // content: '<h1>The Getty Center</h1>'
+                                // },
+                                // {
+                                    //     coords: { lat: 34.0101, lng: -118.4961 },
+                                    //     // content: '<h1>Santa Monica Pier</h1>'
+                                    // }
+                                ];
+                                
+                                function createMarkers (){
+                                    
+                                    for (var i = 0; i < markers.length; i++) {
+                                        // console.log(markers[i]);
+                                        addMarker(markers[i]);
+                                    }
+                                }
+                                createMarkers();
+
+
     // array of markers
-    var markers = [
-        {
-            coords: { lat: 34.0622, lng: -118.4450 },
-            content: '<h1>UCLA Extension</h1>'
-        },
-        {
-            coords: { lat: 34.0780, lng: -118.4741 },
-            content: '<h1>The Getty Center</h1>'
-        },
-        {
-            coords: { lat: 34.0101, lng: -118.4961 },
-            content: '<h1>Santa Monica Pier</h1>'
-        }
-    ];
+
 
     // loop through markers
-    for (var i = 0; i < markers.length; i++) {
-        addMarker(markers[i]);
-    }
+
 
     // add marker function
     function addMarker(props) {
+        console.log(props)
         var marker = new google.maps.Marker({
             position: props.coords,
             map: map,
@@ -84,5 +161,6 @@ function initMap() {
             });
         }
     }
-}
 
+
+}
